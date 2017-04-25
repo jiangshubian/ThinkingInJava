64bac6a8f1a590f816678761eb3dc64a203f55c7
jiangshubian <112233b>
2017-04-25 23:23:05 +0800

    seventh--composition and inherit

---
+++ src/main/java/com/shu/seventhsection/Car.java
@@ -0,0 +1,59 @@
+package com.shu.seventhsection;
+
+/**
+ * resing/Car.java
+ * Composition with public objects,in order to show easy to programmer;usually with private.
+ * Created by Administrator on 2017-04-25.
+ */
+public class Car {
+    public Engine engine = new Engine();
+    public Wheel[] wheel = new Wheel[4]; //the car of wheel
+    public Door left = new Door(),right = new Door(); //left door and right door
+
+    public Car() {
+        for(int i=wheel.length-1;i>=0;i--){
+            wheel[i] = new Wheel();
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.left.window.rollup();
+        car.wheel[0].inflate(72);
+        car.engine.service();
+    }
+}
+
+class Engine{
+    public void start(){}
+    public void stop(){}
+    public void reverse(){}
+    public void service(){
+        System.out.println("Engine.service");
+    }
+}
+
+class Wheel{
+    public void inflate(int psi){
+        System.out.println("Wheel.inflate:"+psi);
+    }
+}
+
+class  Window{
+    public void rollup(){
+        System.out.println("Window.rollup");
+    }
+    public void rolldown(){
+        System.out.println("Window.rolldown");
+    }
+}
+
+class Door{
+    public Window window = new Window();
+    public void open(){
+        System.out.println("Door.open");
+    }
+    public void close(){
+        System.out.println("Door.close");
+    }
+}
---
+++ src/main/java/com/shu/seventhsection/Hide.java
@@ -0,0 +1,43 @@
+package com.shu.seventhsection;
+
+/**
+ * reusing/Hide.java
+ * Overloading a base-class method name in a drived
+ * class does not hide the base-class version.
+ * 重载父类同名方法,在重写父类方法时，使用注解@Override来增强重写标识，避免出错.
+ * Created by Administrator on 2017-04-25.
+ */
+public class Hide {
+    public static void main(String[] args) {
+        Bart b = new Bart();
+        b.doh(1);
+        b.doh('x');
+        b.doh(10.0f);
+        b.doh(new Milhouse());
+    }
+}
+
+class Homer{
+    char doh(char c){
+        System.out.println("Home.doh(char)");
+        return 'a';
+    }
+    float doh(float f){
+        System.out.println("Home.doh(float)");
+        return 1.0f;
+    }
+}
+
+class Milhouse{}
+
+class Bart extends Homer{
+    void doh(Milhouse m){
+        System.out.println("Bart.doh(Milhouse)");
+    }
+    @Override
+    char doh(char c){
+        super.doh(c);
+        System.out.println("Bart.doh(char)");
+        return 'b';
+    }
+}
