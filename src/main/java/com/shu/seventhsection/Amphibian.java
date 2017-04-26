3acc9a79648d7c92ab39a91679facca12991779e
jiangshubian <112233b>
2017-04-26 21:07:18 +0800

    seventh--expand composition and inherit

---
+++ src/main/java/com/shu/seventhsection/Amphibian.java
@@ -0,0 +1,35 @@
+package com.shu.seventhsection;
+
+/**
+ * base class
+ * Created by Administrator on 2017-04-26.
+ */
+public class Amphibian {
+    /**
+     * a name
+     */
+    protected String name;
+
+    /**
+     * constructor
+     * @param name the name what you want to set.
+     */
+    public Amphibian(String name) {
+        this.name = name;
+    }
+
+    /**
+     * the get name method for all subclass
+     * @return the name what subclass set
+     */
+    protected String getName(){
+        return name;
+    }
+
+    /**
+     * print test message for this method
+     */
+    public void doSomethings(){
+        System.out.println("Today is rainning....");
+    }
+}
---
+++ src/main/java/com/shu/seventhsection/Frog.java
@@ -0,0 +1,44 @@
+package com.shu.seventhsection;
+
+/**
+ * subclass
+ * Created by Administrator on 2017-04-26.
+ */
+public class Frog extends Amphibian {
+    /**
+     * constructor
+     * @param name the name what you want to set.
+     */
+    public Frog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void doSomethings() {
+        System.out.println("Frog.doSomethings");
+    }
+
+    public void  mySelfMethod(){
+        System.out.println("Frog.mySelfMethod");
+    }
+    /**
+     * main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        //向上转型 无法调用子类特有方法
+        Amphibian f = new Frog("shu");
+        System.out.println(f.getName()); //shu
+        f.doSomethings(); //Frog.doSomethings
+
+        //向下转型
+        if(f instanceof Frog)((Frog)f).mySelfMethod();//调用子类自有方法
+    }
+
+
+}
+/*
+    shu
+    Frog.doSomethings
+    Frog.mySelfMethod
+* */
